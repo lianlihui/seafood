080b4116c47ba86b1d2ee9aa7d30baae26ef8171
TracerLee <haizhen.li@outlook.com>
2017-09-14 23:22:09 +0800

    fix bug 修改下单页面的交互

--- pages/orderdetail/orderdetail.js
+++ pages/orderdetail/orderdetail.js
@@ -192,5 +192,16 @@ Page({
         console.log(res);
       }
     })
+  },
+
+  // 选择地址
+  goAddressList () {
+    let url = '/pages/address/list/list?select=1'
+
+    if (this.data.addressid) {
+      url += '&curid=' + this.data.addressid
+    }
+
+    wx.navigateTo({url})
   }
 })
